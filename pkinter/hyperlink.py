881bc9ed6b74f5110973c435447753e5577556ba
DeflatedPickle <DeflatedPickle@gmail.com>
2017-01-11 23:00:28 +0000

    Updated widget
    Updated the hyperlink widget.

--- pkinter/hyperlink.py
+++ pkinter/hyperlink.py
@@ -6,27 +6,37 @@ import webbrowser
 #link
 
 __title__ = "Hyperlink"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "DeflatedPickle"
 
 class Hyperlink (ttk.Label):
     """
             -----DESCRIPTION-----
+    A modification of the TTK label to act as a hyperlink.
+    It can take the user to any given website with a click.
 
             -----USAGE-----
+    hyperlink = Hyperlink (parent, link = [string])
+    hyperlink.pack ()
 
             -----CONTENTS-----
     ---VARIABLES---
+    link               = Holds the website to go to.
+    font               = The font of the label.
 
     ---WIDGETS---
     Self
 
     ---FUNCTIONS---
+    enter ()           = Runs when the mouse enters the widget.
+    leave ()           = Runs when the mouse leaves the widget.
+    button ()          = Runs when the user clicks the widget.
+    button_released () = Runs when the user stops clicking on the widget.
     """
-    def __init__ (self, parent, link = "www.website.com", *args):
+    def __init__ (self, parent, link = "https://github.com/DeflatedPickle/pkinter", *args):
         ttk.Label.__init__ (self, parent, text = link, foreground = "blue", cursor = "arrow", *args)
         self.link = link
-        self.font = font.Font ()
+        self.font = font.Font (self, self.cget ("font"))
         self.configure (font = self.font)
 
         self.bind ("<Enter>", self.enter)
@@ -36,21 +46,25 @@ class Hyperlink (ttk.Label):
 
     def enter (self, *args):
         self.configure (foreground = "dodgerblue", cursor = "hand2")
+        self.font.configure (underline = True)
 
     def leave (self, *args):
         self.configure (foreground = "blue", cursor = "arrow")
+        self.font.configure (underline = False)
 
     def button (self, *args):
         self.configure (foreground = "medium blue", cursor = "arrow")
+        self.font.configure (underline = True)
 
     def button_released (self, *args):
         self.configure (foreground = "dodgerblue", cursor = "arrow")
+        self.font.configure (underline = False)
         webbrowser.open_new_tab (self.link)
 
 ##################################################
 
 if __name__ == "__main__":
     root = tk.Tk ()
-    hlink = Hyperlink (root)
+    hlink = Hyperlink (root, link = "https://github.com/DeflatedPickle/pkinter")
     hlink.grid (row = 0, column = 0, padx = 5, pady = 5)
     root.mainloop ()
