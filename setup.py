a881444c665374fc1c464fd957636d1345c0f790
DeflatedPickle <DeflatedPickle@gmail.com>
2017-01-13 21:53:37 +0000

    Added files
    Preparing to upload to PyPi.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (name = "Pkinter",
+      version = "1.8.1",
+      description = "A set of useful GUI widgets inherited from TkInter.",
+      url = "https://github.com/DeflatedPickle/pkinter",
+      author = "DeflatedPickle",
+      author_email = "DeflatedPickle@gmail.com",
+      license = "MIT",
+      packages = ["pkinter"],
+      zip_safe = False)
