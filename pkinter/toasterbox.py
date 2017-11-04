05eec8c1e1fe316594215ef5ea29f504337e9a93
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-04 23:07:03 +0000

    Automated placement of the window

--- pkinter/toasterbox.py
+++ pkinter/toasterbox.py
@@ -8,7 +8,7 @@ from tkinter import ttk
 # link
 
 __title__ = "ToasterBox"
-__version__ = "1.1.5"
+__version__ = "1.1.0"
 __author__ = "DeflatedPickle"
 
 
@@ -36,9 +36,20 @@ class ToasterBox(tk.Toplevel):
     ---FUNCTIONS---
     None
     """
-    def __init__(self, parent, *args):
+    def __init__(self, parent, minus_width=5, minus_height=5, extra_height=40, *args):
         tk.Toplevel.__init__(self, parent, *args)
         self.parent = parent
+        self._minus_width = minus_width
+        self._minus_height = minus_height
+        self._extra_height = extra_height
+
+        self.attributes("-toolwindow", True, "-topmost", True, "-disable", True)
+        self.overrideredirect(True)
+
+        self.update()
+
+        self.geometry("+{}+{}".format((self.winfo_screenwidth() - self.winfo_width()) - self._minus_width,
+                                      (self.winfo_screenheight() - self.winfo_height()) - self._extra_height - self._minus_height))
 
 ##################################################
 
