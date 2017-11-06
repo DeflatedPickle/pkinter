7fcf997d6bff102313f23d4f58228aec919ba066
Dibbo <DeflatedPickle@gmail.com>
2017-11-06 11:39:22 +0000

    Changed the handle to use the bbox height

--- pkinter/documentmap.py
+++ pkinter/documentmap.py
@@ -8,7 +8,7 @@ from tkinter import ttk
 # link
 
 __title__ = "DocumentMap"
-__version__ = "1.6.0"
+__version__ = "1.6.1"
 __author__ = "DeflatedPickle"
 
 
@@ -50,7 +50,10 @@ class DocumentMap(tk.Canvas):
 
         self._widget_text = self.create_text([self._text_pad, self._text_pad], text=self._text_widget.get(1.0, "end"), font=self._text_font, anchor="nw", tags="text")
 
-        self._handle = self.create_rectangle([0, 0, self._width, 70], fill=self._scroll_fill, width=0, stipple="gray25", tags="handle")
+        self._text_widget.update()
+        self._box = self._text_widget.bbox(1.0)
+        self._handle = self.create_rectangle([0, self._box[1], self._width, self._box[3]], fill=self._scroll_fill, width=0, stipple="gray25", tags="handle")
+
         self.tag_bind("handle", "<Enter>", lambda event=None: self.configure(cursor="hand2"), "+")
         self.tag_bind("handle", "<Leave>", lambda event=None: self.configure(cursor="arrow"), "+")
 
@@ -71,7 +74,7 @@ class DocumentMap(tk.Canvas):
         self.itemconfigure(self._widget_text, text=self._text_widget.get(1.0, "end"))
 
     def _move(self, event=None):
-        self.coords(self._handle, self.coords(self._handle)[0], event.y - 35, self.coords(self._handle)[0] + self._width, event.y + 35)
+        self.coords(self._handle, self.coords(self._handle)[0], event.y - self._box[3] / 2, self.coords(self._handle)[0] + self._width, event.y + self._box[3] / 2)
 
     def _smooth_collide(self, interval=60):
         if self.coords(self._handle)[1] <= 0:
@@ -87,11 +90,11 @@ class DocumentMap(tk.Canvas):
     def _harsh_collide(self, interval=1, event=None):
         if self.coords(self._handle)[1] <= 0:
             c = self.coords(self._handle)
-            self.coords(self._handle, c[0], 0, c[2], 70)
+            self.coords(self._handle, c[0], 0, c[2], self._box[3])
 
         elif self.coords(self._handle)[3] >= self.winfo_height():
             c = self.coords(self._handle)
-            self.coords(self._handle, c[0], self.winfo_height() - 70, c[2], self.winfo_height())
+            self.coords(self._handle, c[0], self.winfo_height() - self._box[3], c[2], self.winfo_height())
 
         self.after(interval, self._harsh_collide)
 
@@ -100,7 +103,7 @@ class DocumentMap(tk.Canvas):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    text = tk.Text(root, width=1, height=1)
+    text = tk.Text(root, width=1, height=1, wrap="none")
     text.pack(side="left", fill="both", expand=True, padx=[5, 0], pady=5)
     for i in range(0, 100):
         text.insert("{}.0".format(i), "{}\n".format(i))
