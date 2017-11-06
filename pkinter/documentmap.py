47b75a09d9858a560ad7903a8de9dac678bea481
Dibbo <DeflatedPickle@gmail.com>
2017-11-06 10:21:51 +0000

    Added a harsh collide

--- pkinter/documentmap.py
+++ pkinter/documentmap.py
@@ -8,7 +8,7 @@ from tkinter import ttk
 # link
 
 __title__ = "DocumentMap"
-__version__ = "1.5.0"
+__version__ = "1.6.0"
 __author__ = "DeflatedPickle"
 
 
@@ -37,7 +37,7 @@ class DocumentMap(tk.Canvas):
     ---FUNCTIONS---
     None
     """
-    def __init__(self, parent, text_widget, scroll_widget=None, scroll_fill="orange", text_font=("courier", 7), text_pad=5, background="white", width=170, *args):
+    def __init__(self, parent, text_widget, scroll_widget=None, scroll_fill="orange", text_font=("courier", 7), text_pad=5, collide="harsh", background="white", width=170, *args):
         tk.Canvas.__init__(self, parent, background=background, width=width, *args)
         self.parent = parent
         self._text_widget = text_widget
@@ -45,6 +45,7 @@ class DocumentMap(tk.Canvas):
         self._scroll_fill = scroll_fill
         self._text_font = text_font
         self._text_pad = text_pad
+        self._collide = collide
         self._width = width
 
         self._widget_text = self.create_text([self._text_pad, self._text_pad], text=self._text_widget.get(1.0, "end"), font=self._text_font, anchor="nw", tags="text")
@@ -56,11 +57,15 @@ class DocumentMap(tk.Canvas):
         self.bind("<Button-1>", self._move, "+")
         self.bind("<B1-Motion>", self._move, "+")
 
+        if self._collide == "harsh":
+            self._harsh_collide()
+
+        elif self._collide == "smooth":
+            self._smooth_collide()
+
         self._text_widget.bind("<<Change>>", self._redraw, "+")
         self._text_widget.bind("<Configure>", self._redraw, "+")
         self._text_widget.bind("<KeyRelease>", self._redraw, "+")
-
-        self._collide()
 
     def _redraw(self, event=None):
         self.itemconfigure(self._widget_text, text=self._text_widget.get(1.0, "end"))
@@ -68,7 +73,7 @@ class DocumentMap(tk.Canvas):
     def _move(self, event=None):
         self.coords(self._handle, self.coords(self._handle)[0], event.y - 35, self.coords(self._handle)[0] + self._width, event.y + 35)
 
-    def _collide(self, interval=60):
+    def _smooth_collide(self, interval=60):
         # FIXME: Make this a harsh collision.
         if self.coords(self._handle)[1] <= 0:
             c = self.coords(self._handle)
@@ -78,7 +83,19 @@ class DocumentMap(tk.Canvas):
             c = self.coords(self._handle)
             self.coords(self._handle, c[0], c[1] - 1, c[2], c[3] - 1)
 
-        self.after(interval, self._collide)
+        self.after(interval, self._smooth_collide)
+
+    def _harsh_collide(self, interval=1, event=None):
+        if self.coords(self._handle)[1] <= 0:
+            c = self.coords(self._handle)
+            self.coords(self._handle, c[0], 0, c[2], 70)
+
+        elif self.coords(self._handle)[3] >= self.winfo_height():
+            c = self.coords(self._handle)
+            self.coords(self._handle, c[0], self.winfo_height() - 70, c[2], self.winfo_height())
+
+        self.after(interval, self._harsh_collide)
+
 
 ##################################################
 
